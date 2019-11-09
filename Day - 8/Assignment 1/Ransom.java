9daee1fee368866ccb9f01d7d9750c787d33cbff
rnv19 <routulasundeep@msitprogram.net>
2019-11-09 20:09:11 +0530

    final

--- Day - 8/Assignment 1/Ransom.java
+++ Day - 8/Assignment 1/Ransom.java
@@ -1,9 +1,12 @@
 class Solution {
 
-    public static boolean canMake(String[] magazine, String[] ransom) {
+    public static boolean ransomFind(String[] magazine, String[] ransom) {
         SeparateChainingHashST<String, Integer> st = new SeparateChainingHashST<String, Integer>();
         for (int i = 0; i < magazine.length; i++) {
-            st.put(magazine[i], i+1);
+            if (st.contains(magazine[i])) {
+                int value = st.get(magazine[i]);
+                st.put(magazine[i], value + 1);
+            }else st.put(magazine[i], 1);
         }
         for (int i = 0; i < ransom.length; i++) {
             int val = st.get(ransom[i]);
@@ -19,11 +22,11 @@ class Solution {
     public static void main(String[] args) {
         SeparateChainingHashST<String, Integer> st = new SeparateChainingHashST<String, Integer>();
         String[] magazine = {"it", "was", "best", "of", "times"};
-        String[] ransom = {"it", "was", "best", "of", "times", "it"};
-        System.out.println(canMake(magazine, ransom) ? "Yes" : "No");
-        String[] magazine1 = {"give", "me", "one", "grand", "today", "night"};
-        String[] ransom1 = {"give", "one", "grand", "today"};
-        System.out.println(canMake(magazine1, ransom1) ? "Yes" : "No");
+        String[] ransom = {"it", "was", "best", "of", "times"};
+        System.out.println(ransomFind(magazine, ransom) ? "Yes" : "No");
+        String[] magazine1 = {"hello", "how", "are", "you", "doing", "today"};
+        String[] ransom1 = {"how", "are", "you", "are", "are"};
+        System.out.println(ransomFind(magazine1, ransom1) ? "Yes" : "No");
     }
 
 }
---
+++ Day - 8/Assignment 2/LinearProbingHashST.java
@@ -0,0 +1,108 @@
+public class LinearProbingHashST<Key, Value> {
+    private static final int INIT_CAPACITY = 4;
+
+    private int n;           // number of key-value pairs in the symbol table
+    private int m;           // size of linear probing table
+    private Key[] keys;      // the keys
+    private Value[] vals;    // the values
+
+    public LinearProbingHashST(final int capacity) {
+        m = capacity;
+        n = 0;
+        keys = (Key[])   new Object[m];
+        vals = (Value[]) new Object[m];
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public boolean contains(final Key key) {
+        return get(key) != null;
+    }
+
+    private int hash(final Key key) {
+        return (key.hashCode() & 0x7fffffff) % m;
+    }
+
+    private void resize(final int capacity) {
+        LinearProbingHashST<Key, Value> temp = new LinearProbingHashST<Key, Value>(capacity);
+        for (int i = 0; i < m; i++) {
+            if (keys[i] != null) {
+                temp.put(keys[i], vals[i]);
+            }
+        }
+        keys = temp.keys;
+        vals = temp.vals;
+        m    = temp.m;
+    }
+
+    public void put(final Key key, final Value val) {
+
+        if (val == null) {
+            delete(key);
+            return;
+        }
+
+        // double table size if 50% full
+        if (n >= m/2) resize(2*m);
+
+        int i;
+        for (i = hash(key); keys[i] != null; i = (i + 1) % m) {
+            if (keys[i].equals(key)) {
+                vals[i] = val;
+                return;
+            }
+        }
+        keys[i] = key;
+        vals[i] = val;
+        n++;
+    }
+
+    public Value get(final Key key) {
+        for (int i = hash(key); keys[i] != null; i = (i + 1) % m) {
+            if (keys[i].equals(key)) {
+                return vals[i];
+            }
+        }
+        return null;
+    }
+
+    public void delete(final Key key) {
+        // find position i of key
+        int i = hash(key);
+        while (!key.equals(keys[i])) {
+            i = (i + 1) % m;
+        }
+
+        // delete key and associated value
+        keys[i] = null;
+        vals[i] = null;
+
+        // rehash all keys in same cluster
+        i = (i + 1) % m;
+        while (keys[i] != null) {
+            // delete keys[i] an vals[i] and reinsert
+            Key   keyToRehash = keys[i];
+            Value valToRehash = vals[i];
+            keys[i] = null;
+            vals[i] = null;
+            n--;
+            put(keyToRehash, valToRehash);
+            i = (i + 1) % m;
+        }
+        n--;
+    }
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new Queue<Key>();
+        for (int i = 0; i < m; i++)
+            if (keys[i] != null) {
+                queue.enqueue(keys[i]);
+            }
+        return queue;
+    }
+}
